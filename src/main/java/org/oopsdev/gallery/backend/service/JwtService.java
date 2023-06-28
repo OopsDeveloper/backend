121190e9bf8872ee16eb0f99349a8e499d612896
Go YongSu <itgys91@kbet.or.kr>
2023-06-28 17:28:37 +0900

    로그인 check 로직 보완

--- src/main/java/org/oopsdev/gallery/backend/service/JwtService.java
+++ src/main/java/org/oopsdev/gallery/backend/service/JwtService.java
@@ -1,5 +1,9 @@
 package org.oopsdev.gallery.backend.service;
 
+import io.jsonwebtoken.Claims;
+
 public interface JwtService {
     public String getToken(String key, Object value);
+
+    Claims getClaims(String token);
 }
